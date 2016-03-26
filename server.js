e6a060e766947d0c2a22223a87083cf83423ba13
Paul Bardea <paul@pbardea.com>
2016-03-26 13:15:30 -0400

    New remote db

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var constants = require('./const')
 
-var db = 'mongodb://'+ constants.name +':'+ constants.pword +'@ds013738.mlab.com:13738/scavenger';
-/* var db = 'mongodb://localhost/scavenger' */
+var db = 'mongodb://'+ constants.name +':'+ constants.pword +'@ds023118.mlab.com:23118/scavenger';
+// var db = 'mongodb://localhost/scavenger' 
 
 mongoose.connect(db);
 // Express
